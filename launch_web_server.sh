8c97bf6a1d61c5c282d646bbbd47160906ac9f77
Kyle <kyle@pop-os.localdomain>
2024-11-30 15:36:15 -0800

    created new docker build for backend

--- launch_web_server.sh
+++ launch_web_server.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+# If you're having issues with the image recalling errors from some
+# cache, then remove the image in 
+# /var/lib/docker/image/overlay2/imagedb/content/sha256#
+# that corresponds to the message 'writing image sha256:...'
+
 docker kill nginx
 docker rm nginx
 
 # Build
 docker build --rm --no-cache -t nginx .
+
 # Start the web server
-docker run -dit --name nginx -p 8080:80 nginx
+# docker run -dit --name nginx -p 8080:80 nginx
+docker run -it --name nginx -p 8080:80 nginx
 
